6d4b6c3dc5056b03f3026053ad03bed858037d29
1898ha <94360030+1898ha@users.noreply.github.com>
2022-11-23 16:11:20 +0800

    Add files via upload

---
+++ down_era5.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#Input the parameters
+year_begin=$1
+year_end=$2
+
+echo "Begin to download：$1 - $2 ERA5 data"
+
+for var in `cat vars`
+
+do
+
+	mkdir -p  ./ERA5/${var}
+
+
+	for year in `seq ${year_begin} ${year_end}`
+
+	do
+		for mon in `cat month`
+		do
+			# Naming convention
+			filename=era5.hourly.${var}.${year}.${mon}
+
+# Create specific python script, you can change the script below if you needed.
+			cat>./down_py/${filename}.py<<EOF
+
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type': 'reanalysis',
+        'format': 'netcdf',
+        'variable': [
+            '${var}',
+        ],
+        'year': [
+            '${year}',
+        ],
+        'month': [
+            '${mon}',
+        ],
+        'day': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+            '13', '14', '15',
+            '16', '17', '18',
+            '19', '20', '21',
+            '22', '23', '24',
+            '25', '26', '27',
+            '28', '29', '30',
+            '31',
+        ],
+        'time': [
+            '00:00', '03:00', '06:00',
+            '09:00', '12:00', '15:00',
+            '18:00', '21:00',
+        ],
+    },
+    './ERA5/${var}/${filename}.nc')
+
+EOF
+
+# run the python script and output the log file to specific dir.
+nohup python ./down_py/${filename}.py  > ./log/${filename}.log 2>&1 &
+
+		done
+	
+	done
+
+done
---
+++ monitor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This is the monitor script to monitor the era data download.
+# Create dir
+mkdir -p ERA5
+mkdir -p down_py
+mkdir -p log
+# Set the time period
+year_begin=1990
+year_end=1993
+echo "Open up the monitor"
+# If you should download the data to the other end time, you can change the "2020" to another one.
+while [[ ${year_end} -le 1993 ]];
+do
+  # Call the shell "down_era5.sh"
+	bash down_era5.sh ${year_begin}  ${year_end}
+	echo "sleep 40 min, keep running"
+	# You can use the 'sleep' to adjust the period "year_begin - year_end" download time.
+  sleep 40m  
+	echo "${year_begin}-${year_end} finished!!!!!"
+	# Set the length of the period
+	year_begin=$((${year_begin}+2))
+	year_end=$((${year_end}+2))
+done
+echo "Work has been done!!!"
